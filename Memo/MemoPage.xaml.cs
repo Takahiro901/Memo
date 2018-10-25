2673836c87212420664e881f582ec8ff1d2b1232
Takahiro901 <30286581+Takahiro901@users.noreply.github.com>
2018-10-25 22:54:33 +0900

    検索機能を追加

--- Memo/MemoPage.xaml.cs
+++ Memo/MemoPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using Xamarin.Forms;
+using System.Linq;
 using System.Collections.ObjectModel;
 using Memo.Database;
+using System.Collections.Generic;
 
 namespace Memo
 {
@@ -14,13 +16,32 @@ namespace Memo
         {
             InitializeComponent();
 
-            //listView.ItemsSource = await todoItemDatabase.GetItemsAsync();
-
             listView.ItemTapped += async (s, a) =>
             {
                 var wItem = (TodoItem)a.Item;
                 await Navigation.PushAsync(new EditPage(wItem));
             };
+
+            searchbar.TextChanged += (sender, e) => this.SearchFilter(e.NewTextValue);
+            searchbar.SearchButtonPressed += (sender, e) => this.SearchFilter(searchbar.Text);
+
+        }
+
+
+        public  async void SearchFilter(string filter)
+        {
+            listView.BeginRefresh();
+            
+            if (string.IsNullOrWhiteSpace(filter))
+            {
+                listView.ItemsSource = await todoItemDatabase.GetItemsAsync();
+            }
+            else
+            {
+                listView.ItemsSource = todoItemDatabase.GetItemsAsync().Result.Where(x => x.Memo.ToLower().Contains(filter));
+            }
+
+            listView.EndRefresh();
         }
 
         protected override async void OnAppearing()
